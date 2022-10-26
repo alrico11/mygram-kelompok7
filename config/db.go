628098bc97a0f1eec0ad5d14bfca2f301fa85b39
alrico11 <91462302+alrico11@users.noreply.github.com>
2022-10-26 21:17:59 +0700

    Update db.go

--- config/db.go
+++ config/db.go
@@ -1,8 +1,7 @@
-package conf
+package config
 
 import (
 	"fmt"
-	"os"
 
 	"gorm.io/driver/mysql"
 
@@ -11,14 +10,8 @@ import (
 
 func InitDB() *gorm.DB {
 
-	userName := ("root")
-	password := ("")
-	hostIP := ("localhost")
-	portServer := (":3306")
-	dbName := ("DATABASE")
-
 	// read db
-	dsnString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", userName, password, hostIP, portServer, dbName)
+	dsnString := "root:@tcp(127.0.0.1:3306)/db_belajar_golang?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err := gorm.Open(mysql.Open(dsnString), &gorm.Config{})
 	if err != nil {
