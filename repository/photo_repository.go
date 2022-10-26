73c5e00f092808fdcff28d08f5a949daf51d0a00
alrico11 <91462302+alrico11@users.noreply.github.com>
2022-10-26 21:13:08 +0700

    Add files via upload

--- repository/photo_repository.go
+++ repository/photo_repository.go
@@ -1,1 +1,81 @@
-photo repo
+package repository
+
+import (
+	"project2/model/entity"
+
+	"gorm.io/gorm"
+)
+
+type PhotoRepository interface {
+	Save(photo entity.Photo) (entity.Photo, error)
+	Delete(ID int) (entity.Photo, error)
+	FindByID(ID int) (entity.Photo, error)
+	FindByUserID(ID int) ([]entity.Photo, error)
+	Update(photo entity.Photo, ID int) (entity.Photo, error)
+}
+
+type photoRepository struct {
+	db *gorm.DB
+}
+
+func NewPhotoRepository(db *gorm.DB) *photoRepository {
+	return &photoRepository{db}
+}
+
+func (r *photoRepository) Save(photo entity.Photo) (entity.Photo, error) {
+	err := r.db.Save(&photo).Error
+
+	if err != nil {
+		return entity.Photo{}, err
+	}
+
+	return photo, nil
+}
+
+func (r *photoRepository) FindByID(ID int) (entity.Photo, error) {
+	photo := entity.Photo{}
+
+	err := r.db.Where("id = ?", ID).Find(&photo).Error
+
+	if err != nil {
+		return photo, err
+	}
+
+	return photo, nil
+}
+
+func (r *photoRepository) FindByUserID(ID int) ([]entity.Photo, error) {
+	var photos []entity.Photo
+
+	err := r.db.Where("user_id = ?", ID).Find(&photos).Error
+
+	if err != nil {
+		return []entity.Photo{}, err
+	}
+
+	return photos, nil
+}
+
+func (r *photoRepository) Delete(ID int) (entity.Photo, error) {
+	photoDeleted := entity.Photo{
+		ID: ID,
+	}
+
+	err := r.db.Where("id = ?", ID).Delete(&photoDeleted).Error
+
+	if err != nil {
+		return entity.Photo{}, err
+	}
+
+	return photoDeleted, err
+}
+
+func (r *photoRepository) Update(photo entity.Photo, ID int) (entity.Photo, error) {
+	err := r.db.Where("id = ?", ID).Updates(&photo).Error
+
+	if err != nil {
+		return entity.Photo{}, err
+	}
+
+	return photo, nil
+}
--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,1 +1,88 @@
-repo user
+package repository
+
+import (
+	"project2/model/entity"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Save(user entity.User) (entity.User, error)
+	FindByEmail(email string) (entity.User, error)
+	FindByID(ID int) (entity.User, error)
+	Update(ID int, user entity.User) (entity.User, error)
+	// Update(user entity.User) (entity.User, error)
+	Delete(ID int) (entity.User, error)
+	// Get(ID int) (entity.User, error)
+	// GetAll() ([]entity.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *userRepository {
+	return &userRepository{db}
+}
+
+func (r *userRepository) Save(user entity.User) (entity.User, error) {
+	err := r.db.Create(&user).Error
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *userRepository) FindByEmail(email string) (entity.User, error) {
+	user := entity.User{}
+
+	err := r.db.Where("email = ?", email).Find(&user).Error
+
+	if err != nil {
+		return user, err
+	}
+
+	if user.ID == 0 {
+		return entity.User{}, nil
+	}
+
+	return user, nil
+}
+
+func (r *userRepository) FindByID(ID int) (entity.User, error) {
+	user := entity.User{}
+
+	err := r.db.Where("id = ?", ID).Find(&user).Error
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *userRepository) Update(ID int, user entity.User) (entity.User, error) {
+	err := r.db.Where("id = ?", ID).Updates(&user).Error
+
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	return user, nil
+}
+
+func (r *userRepository) Delete(ID int) (entity.User, error) {
+	userDeleted := entity.User{
+		ID: ID,
+	}
+
+	err := r.db.Where("id = ?", ID).Delete(&userDeleted).Error
+
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	return userDeleted, err
+}
