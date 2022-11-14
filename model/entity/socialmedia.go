8ccfc06a95f06210ba4a6a517f933c0f5548dedb
alrico11 <91462302+alrico11@users.noreply.github.com>
2022-11-15 04:20:19 +0700

    Update socialmedia.go

--- model/entity/socialmedia.go
+++ model/entity/socialmedia.go
@@ -3,12 +3,12 @@ package entity
 import "time"
 
 type SocialMedia struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	URL       string    `json:"social_media_url"`
-	UserID    int       `json:"user_id"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"-"`
+	ID        int    
+	Name      string  
+	URL       string    
+	UserID    int     
+	CreatedAt time.Time 
+	UpdatedAt time.Time 
 	User      User
 }
 
