f77642ba9cd63f3bdcc1c1dfe0fbc858f3916a85
alrico11 <91462302+alrico11@users.noreply.github.com>
2023-01-07 01:40:18 +0700

    Update socialmedia.go

--- model/entity/socialmedia.go
+++ model/entity/socialmedia.go
@@ -3,13 +3,13 @@ package entity
 import "time"
 
 type SocialMedia struct {
-	ID        int    
+	ID        int  `gorm:"primaryKey"`
 	Name      string  
 	URL       string    
 	UserID    int     
 	CreatedAt time.Time 
 	UpdatedAt time.Time 
-	User      User
+	User      User `gorm:"foreignKey:UserID;Constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 func (SocialMedia) TableName() string {
