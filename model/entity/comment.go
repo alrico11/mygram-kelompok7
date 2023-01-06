7bddce742aebb5639bbe716f59932668c49c7f34
alrico11 <91462302+alrico11@users.noreply.github.com>
2023-01-07 01:39:35 +0700

    Update comment.go

--- model/entity/comment.go
+++ model/entity/comment.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Comment struct {
-	ID        int      
+	ID        int  `gorm:"primaryKey"`
 	UserID    int      
 	PhotoID   int  
 	Message   string  
 	CreatedAt time.Time 
 	UpdatedAt time.Time
-	User      User
-	Photo     Photo
+	User      User `gorm:"foreignKey:UserID;Constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Photo     Photo `gorm:"foreignKey:PhotoID;Constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 func (Comment) TableName() string {
